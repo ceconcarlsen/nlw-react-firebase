1371b5c8240fae68ca1908c6f19039d8d827aba4
ceconcarlsen <ceconcarlsen@gmail.com>
2021-10-15 23:46:40 -0300

    aula-03

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,16 @@ import { Home } from "./pages/Home";
 import { NewRoom } from "./pages/NewRoom";
 import { Room } from "./pages/Room";
 
+import { Toaster } from "react-hot-toast";
+
 export const App = () => {
   return (
     <BrowserRouter>
+      <Toaster />
       <AuthContextProvider>
-        <Switch> {/* previne que duas rotas sejam acessadas ao mesmo tempo*/}
+        <Switch>
+          {" "}
+          {/* previne que duas rotas sejam acessadas ao mesmo tempo*/}
           <Route path="/" exact component={Home} />
           <Route path="/rooms/new" component={NewRoom} />
           <Route path="/rooms/:id" component={Room} />
